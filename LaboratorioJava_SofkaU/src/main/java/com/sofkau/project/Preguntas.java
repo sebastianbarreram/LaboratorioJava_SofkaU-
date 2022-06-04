23eafd9e7a22608ab79da60ed944ab060f53234e
sebastianbarreram <88840800+sebastianbarreram@users.noreply.github.com>
2022-06-04 02:07:44 -0500

    preguntas con sonarlint

--- LaboratorioJava_SofkaU/src/main/java/com/sofkau/project/Preguntas.java
+++ LaboratorioJava_SofkaU/src/main/java/com/sofkau/project/Preguntas.java
@@ -1,68 +1,48 @@
 package com.sofkau.project;
 
 import java.io.*;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Random;
+import java.util.logging.Logger;
 
 public class Preguntas {
-
+    private Logger logger = Logger.getLogger("logger");
     private JSONArray datos;
-
-    private String link = "C:\\Users\\Acer\\Desktop\\LaboratorioJava_SofkaU-\\LaboratorioJava_SofkaU\\";
 
     private Random random = new Random();
 
-    public void cargarDatos() throws IOException {
-        String archivo = "data.json";
-        String data = new String(Files.readAllBytes(Paths.get(this.link + archivo)));
+    public void cargarDatos(){
+        try{
+            String link = "C:\\Calculadora\\LaboratorioJava_SofkaU-\\LaboratorioJava_SofkaU\\";
+            String archivo = "data.json";
+            String data = new String(Files.readAllBytes(Paths.get(link + archivo)));
 
-        // leer como un json array
-        this.datos = new JSONArray(data);
-        for (int i = 0; i < this.datos.length(); i++) {
-            // llevarlo a un json object
-            String str = this.datos.get(i).toString();
-            JSONObject object1 = new JSONObject(str);
-
-            // String name = Integer.toString(object1.getInt("categoria"));
-            // String age = object1.getString("pregunta");
-            // System.out.println("pregunta: " + name);
-
-            // System.out.println("Respuesta: " + age);
-            /*
-             * System.out.println(i);
-             */
+            // leer como un json array
+            this.datos = new JSONArray(data);
+        }catch (IOException e){
+            logger.info(()->"Error "+e);
         }
-
     }
 
     public JSONObject preguntaAleatoria(int nivel) {
         int auxiliar = 0;
-
-        int valorAleatorio = this.random.nextInt(5)+1;
-        System.out.println("valorAleatorio"+valorAleatorio);
+        cargarDatos();
+        int valorAleatorio = this.random.nextInt(4)+2;
         for (int i = 0; i < this.datos.length(); i++) {
             String str = this.datos.get(i).toString();
             JSONObject object1 = new JSONObject(str);
             int categoria = object1.getInt("categoria");
-            //System.out.println("categoria"+categoria);
-            // System.out.println("nivel-1"+(nivel));
-            
             if (categoria == (nivel)) {
-                //System.out.println(str);
                 auxiliar += 1;
             }
-            // System.out.println("valorAleatorio"+valorAleatorio);
-            // System.out.println("auxiliar"+auxiliar);
-            
             if (valorAleatorio == auxiliar) {
                 return new JSONObject(this.datos.get(i).toString());
-                
             }
-            
         }
         return null;
     }
